f951bab28c32c897a3dcb5bf3666a0da678826a5
AlenAvagyan77 <alen_avagyan_03@mail.ru>
2023-04-19 17:15:30 +0400

    Adding new changes, DuplicatesElement, Permutations Of Given String

--- src/LandOfLogic/SumUpNumbers.java
+++ src/LandOfLogic/SumUpNumbers.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class SumUpNumbers {
 
   public static void main(String[] args) {
-    System.out.println(solution("I have buy 2 eeg, 3 bread"));
+    System.out.println(solution("I have buy 2 eeg, 3 bread and 1 Coca-Cola"));
   }
 
   static int solution(String inputString) {
